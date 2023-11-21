5df3e05d6b5dcf5d5c297e8d842e46b766c08fa0
Mikhail Tsarev <de1phin@kakao.com>
2023-11-21 20:31:55 +0300

    fix cache

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -15,10 +15,26 @@ func (s *Cache[Key, Data]) Get(key Key) (Data, bool) {
 	return data, ok
 }
 
-func (s *Cache[Key, Data]) Set(key Key, data Data) {
+func (s *Cache[Key, Data]) Create(key Key, data Data) bool {
+	if _, ok := s.Cache[key]; ok {
+		return false
+	}
 	s.Cache[key] = data
+	return true
 }
 
-func (s *Cache[Key, Data]) Delete(key Key) {
+func (s *Cache[Key, Data]) Update(key Key, data Data) bool {
+	if _, ok := s.Cache[key]; !ok {
+		return false
+	}
+	s.Cache[key] = data
+	return true
+}
+
+func (s *Cache[Key, Data]) Delete(key Key) bool {
+	if _, ok := s.Cache[key]; !ok {
+		return false
+	}
 	delete(s.Cache, key)
+	return true
 }
